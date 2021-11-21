f5138967597a31b91d5dfe7b6f480a32dd40fc5e
Berk Forbes <berkforbes@yahoo.com>
2021-11-21 13:18:36 -0700

    style update w bootstrap and google fonts. layout update to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
-let startBtn = document.querySelector('#start-btn')
+let startBtn = document.getElementById('start-btn')
 let highScoresBtn = document.querySelector('#highscores-btn')
+let quizModal = document.getElementById('quizModal')
+let exit = document.getElementsByClassName('#close')[0];
+
 
 // add high scores URL to activate high score button click highScoresBtn.addEventListener("click", location."highscores.html" )
 
-/* Add click function to start button to open modal? 
-startBtn.addEventListener("click", )*/
+// Start button click to open quiz modal
+startBtn.onclick = function() {
+  quizModal.style.display = "block";
+}
+
+// X click will close quiz modal
+exit.onclick = function() {
+  quizModal.style.display = "none";
+}
+
+
 
