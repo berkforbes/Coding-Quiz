22e9d466cebc8ce7d02f1bc6b157d645698d69a5
Berk Forbes <berkforbes@yahoo.com>
2021-11-21 01:22:34 -0700

    button styles, modal html, js va/functions for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let startBtn = document.querySelector('#start-btn')
+let highScoresBtn = document.querySelector('#highscores-btn')
+
+// add high scores URL to activate high score button click highScoresBtn.addEventListener("click", location."highscores.html" )
+
+/* Add click function to start button to open modal? 
+startBtn.addEventListener("click", )*/
+
